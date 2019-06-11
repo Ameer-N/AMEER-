09b476de31e22e496e96007635203951b2a596a2
Ameer-N <46894200+Ameer-N@users.noreply.github.com>
2019-06-11 09:22:42 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -4,14 +4,14 @@ int main(void) {
 	// your code goes here
 	int a;
 	scanf("%d",&a);
-	if(a>=0)
+	if(a>0)
 	{
 		printf("Positive");
 	}else if(a<0)
 	{
 		printf("Negative");
 	}
-	else 
+	else if(a=0)
 	{
 		printf("Zero");
 	}
