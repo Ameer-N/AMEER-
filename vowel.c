0677fdd1643152a6ae75e4dfee3b38d11a07c374
Ameer-N <46894200+Ameer-N@users.noreply.github.com>
2019-06-11 09:38:00 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main()
+{
+  char ch;
+  scanf("%c",ch);
+  if((ch>='A' && ch<='Z')|| (ch>='a' && ch<='z'))
+  {
+    printf("Vowel");
+  }
+  else
+  {
+    printf("invalid1");
+  }
+}
