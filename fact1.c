8c9c920daf7696a17f932f946017ce21b79bd1b2
Ameer-N <46894200+Ameer-N@users.noreply.github.com>
2019-06-11 21:44:26 +0530

    Create fact1.c

---
+++ fact1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact(int no)
+{
+if(no==0)
+  return 1;
+else
+   return no*fact(no-1);
+}
+int main()
+{
+int no,i;
+scanf("%d",&no);
+if(no>0)
+{
+i=fact(no);
+printf("%d",i);
+
+}
+}
