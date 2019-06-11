b702aa1ab4038535f37bc1b15b5ec85234b518d2
Ameer-N <46894200+Ameer-N@users.noreply.github.com>
2019-06-11 13:37:08 +0530

    Create rangeadd.c

---
+++ rangeadd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int arr[1000],a,b,i,j;
+scanf("%d %d",&a,&b);
+for(i=0;i<a;i++)
+{
+scanf("%d",&arr[i]);
+for(j=0;j<b;j++)
+{
+
+sum+=arr[j];
+}
+}
+printf("%d',sum);
+}
