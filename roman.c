9807ec72b5e0b3b2a1605ee88cd418ab348d91e4
Ameer-N <46894200+Ameer-N@users.noreply.github.com>
2019-06-11 11:44:12 +0530

    Create roman.c

---
+++ roman.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100];
+int i,sum=0;
+scanf("%s",a);
+for(i=0;a[i];i++)
+{
+if(a[0]=='X' || a[i]=='x')
+{
+x=10;
+}
+else if(a[0]!='X' || a[i]=='x')
+{
+x=8;
+}
+if(a[i]=='I' || a[i]=='i')
+  sum=sum+1;
+else if(a[i]=='V' || a[i]=='v')
+  sum=sum+5;
+else if(a[i]=='x' || a[i]=='X')
+  sum=sum+x;
+  
+
+}
+printf("%d",sum);
+
+}
