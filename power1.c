4039c53b6d2d9802122f99857e5647fecad5dd64
Ameer <46894200+Ameer-N@users.noreply.github.com>
2019-08-05 21:06:29 +0530

    Rename power.c to power1.c

---
+++ power1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int isPowerOfTwo(int n)
+{
+if (n == 0)
+	return 0;
+while (n != 1)
+{
+	if (n%2 != 0)
+		return 0;
+	n = n/2;
+}
+return 1;
+}
+
+
+int main()
+{
+int no;
+scanf("%d",&no);
+isPowerOfTwo(no)? printf("Yes\n"): printf("No\n");
+
+return 0;
+}
