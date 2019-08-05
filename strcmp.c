1281e842e1b58394b058a8cb6e35ad998d6bf0f9
Ameer <46894200+Ameer-N@users.noreply.github.com>
2019-08-05 20:00:35 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[1001],str1[1001];
+    scanf("%s %s",str,str1);
+    (strlen(str)>=strlen(str1))?printf("%s",str):printf("%s",str);
+}
