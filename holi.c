4b44b44e8189f48b3dc12d0cab4c9d2ea7b6058a
Ameer <46894200+Ameer-N@users.noreply.github.com>
2019-08-08 18:46:11 +0530

    Create holi.c

---
+++ holi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char str1[20];
+   char str2[20];
+   int result;
+   while(scanf("%s",str1)>0)
+    {
+
+   if(strcmp(str1,"Saturday")==0 || strcmp(str1,"Sunday")==0 ) {
+      printf("yes\n");
+   }
+  else {
+      printf("no\n");
+   }
+    }
+
+   return 0;
+}
