895714eb535ebb8a215f4edc612d69695f38bdb0
Ameer <46894200+Ameer-N@users.noreply.github.com>
2019-08-15 12:30:38 +0530

    Create reversestr.c

---
+++ reversestr.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[1001];
+    int i;
+    while(scanf("%s",str)>0)
+    {
+        for(i=strlen(str)-1;i>=0;i--)
+        {
+            printf("%c",str[i]);
+        }
+        printf(" ");
+    }
+}
+
