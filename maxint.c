4a55eafb5792f7c4e7f6cfe231689f9a494107b2
Ameer <46894200+Ameer-N@users.noreply.github.com>
2019-08-15 11:56:24 +0530

    Update maxint.c

--- maxint.c
+++ maxint.c
@@ -4,23 +4,6 @@
 #include<limits.h>
 int main()
 {
-int n,max=INT_MIN,i,j,swap,place=10,no1;
-scanf("%d",&n);
-int no[n];
-
-for(i=0;i<n;i++)
-{
-    scanf("%d",&no[i]);
-    
-}
-
-for(i=0;i<n-1;i++)
-{
-
-#include <stdio.h>
-#include<limits.h>
-int main()
-{
 int n,max=INT_MIN,i,j,swap,place=10,no1=0;
 scanf("%d",&n);
 int no[n];
@@ -51,24 +34,4 @@ for(i=0;i<n;i++)
 
 }
     printf("%d",no1);
-}
-    for(j=i+1;j<n;j++)
-    {
-        if(no[i]<no[j])
-        {
-            swap=no[i];
-            no[i]=no[j];
-            no[j]=swap;
-        }
-    }
-    //printf("%d",no[i]);
-}
-
-
-for(i=0;i<n;i++)
-{
-    no1==no[i]+no1*place;
-}
-
-printf("%d",no1);
 }
